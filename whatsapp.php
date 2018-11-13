34b5c3b7361993ea3c6f35cf9b6cdc6d6b266e28
oguzturk <45005980+oguzturk@users.noreply.github.com>
2018-11-13 17:01:06 +0300

    Create whatsapp.php

---
+++ whatsapp.php
@@ -0,0 +1,4 @@
+<div class="wtsp" style="position:fixed;bottom:13px;left:13px;z-index:9999;">
+<a href="https://api.whatsapp.com/send?phone=905550001122&amp;text=Sipariş&nbsp;vermek&nbsp;istiyorum. {{ heading_title }} - {{ model }}">
+<img src="image/catalog/whatsapp.gif"></a>
+</div>
